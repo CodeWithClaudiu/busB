d4cc791ebe2769d51ca7189c6101f3856af21a2c
CodeWithClaudiu <claudiochiticaru77@gmail.com>
2026-03-24 14:06:42 +0100

    Implementazione Stop

---
+++ src/main/java/com/generation/bus/dto/StopDTO.java
@@ -0,0 +1,23 @@
+package com.generation.bus.dto;
+
+import java.time.LocalTime;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class StopDTO {
+
+    private Long id;
+    @NotBlank(message = "City is required")
+    private String city;
+    @NotBlank(message = "Address is required")
+    private String address;
+    @NotNull(message = "Position is required")
+    private Integer position;
+    @NotNull(message = "Time is required")
+    private LocalTime time;
+
+    private Long lineId;
+}
---
+++ src/main/java/com/generation/bus/model/Stop.java
@@ -0,0 +1,28 @@
+package com.generation.bus.model;
+import java.time.LocalTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Stop {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String city;      
+    private String address;   
+    private Integer position; 
+    private LocalTime time;     
+
+    @ManyToOne
+    @JoinColumn(name = "line_id")
+    private Line line;
+}
